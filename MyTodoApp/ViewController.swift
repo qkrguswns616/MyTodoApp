14ee40e377043e5fe601c01a6f58bd87b17c9bf7
HyunJun Park <qkrguswns616@gmail.com>
2020-09-29 19:09:33 +0900

    1. Divide into two using multiplier at the bottom of the screen 2. Show date when starting the app 3. Move to another screen when clicking the button

--- MyTodoApp/ViewController.swift
+++ MyTodoApp/ViewController.swift
@@ -9,8 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var date = Date()
+    let dateFormatter = DateFormatter()
+
+    @IBOutlet var dateLebel: UILabel!
+    
+    @IBAction func settingButton(_ sender: Any) {
+        self.dateLebel.text = "TEST"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dateFormatter.dateStyle = .full
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        
+        
+        self.dateLebel.text = dateFormatter.string(from: date)
         // Do any additional setup after loading the view.
     }
 
